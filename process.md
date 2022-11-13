891bde771c7f70c1218f6d44ee0e6ae180fb84a1
Nancy Yoder <nayo4039@colorado.edu>
2022-11-12 17:13:03 -0700

    added some stuff

--- process.md
+++ process.md
@@ -15,3 +15,6 @@ Component Hierarchy
         - CheckBox
 
 Everything will hold all of the other components. ItemTable will hold Subheading, ItemRow, and CheckBox. SortByBox will determine how the ItemRows are organized. CheckBox and SortByBox will take in user input. ItemRow, CheckBox, and SortByBox will change apperance (or order) in response to user input. 
+
+State
+I will need to keep state of the user input: the radio buttons and check boxes. The radio buttons state will be stored in SortByBox, and accessed by Everything to make sure the data is filtered correctly. The check boxes state will be stored in Check Box and change apperance based on user input. 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const ITEMS = [
+  {category: 'School', item: 'Jazz Post Discussion', due: 2022-13-11, done: true},
+  {category: 'School', item: 'Assignment 6', due: 2022-17-11, done: false},
+  {category: 'Work', item: 'Spanish Translation of Action Page', due: 2022-18-11, done: true},
+  {category: 'Work', item: 'Design Impact Statement', due: 2022-20-11, done: false},
+  {category: 'Work', item: 'Redesign of Billboard', due: 2022-22-11, done: false},
+  {category: 'Personal', item: 'Pay Credit Card Bill', due: 2022-25-11, done: false},
+  {category: 'Personal', item: 'Christmas Shop', due: 2022-30-11, done: false},
+  
+];
+class SubHeading extends React.Component {
+  render() {
+    const category = this.props.category;
+    return (
+      <tr>
+        <th colSpan="2">
+          {category}
+        </th>
+      </tr>
+    );
+  }
+}
+
+class CheckBox extends React.Component {
+  render() {
+    const done = this.props.done;
+    const box : 
+      <span>
+    if(done === true){
+
+    }
+    else{
+
+    }
+    return(
+
+    );
+  }
+  
+}
+
+class ItemRow extends React.Component {
+  render() {
+    const item = this.props.item;
+    const dueDate = this.props.due;
+
+    return (
+      <tr>
+        <td>{item}</td>
+        <td>{dueDate}</td>
+      </tr>
+    );
+  }
+}
+
 
 function App() {
   return (
